fea8e6aacf834b7c3a56a7338244890837184df3
hmslima <hmslima@bol.com.br>
2020-03-14 20:43:18 -0300

    A very small, but very important correction

--- manual_transcriptions.h
+++ manual_transcriptions.h
@@ -6,12 +6,12 @@ if (strcmp(argument, "suggest") == 0)
 else if (strcmp(argument, "comprehend") == 0)
 {
 	if (show_system_messages) printf("\npoint ready_made_solution\n");
-	strcpy(SPT_word, "komprëhEnd");
+	strcpy(SPT_word, "kompr" SMALL_E_DIAERESIS "hEnd");
 }
 else if (strcmp(argument, "decret") == 0)
 {
 	if (show_system_messages) printf("\npoint ready_made_solution\n");
-	strcpy(SPT_word, "dëkrEt");
+	strcpy(SPT_word, "d" SMALL_E_DIAERESIS "krEt");
 }
 else if (strcmp(argument, "fortrehc") == 0)
 {
@@ -21,22 +21,22 @@ else if (strcmp(argument, "fortrehc") == 0)
 else if (strcmp(argument, "negleg") == 0)
 {
 	if (show_system_messages) printf("\npoint ready_made_solution\n");
-	strcpy(SPT_word, "nëglEg");
+	strcpy(SPT_word, "n" SMALL_E_DIAERESIS "glEg");
 }
 else if (strcmp(argument, "respect") == 0)
 {
 	if (show_system_messages) printf("\npoint ready_made_solution\n");
-	strcpy(SPT_word, "rëspEkt");
+	strcpy(SPT_word, "r" SMALL_E_DIAERESIS "spEkt");
 }
 else if (strcmp(argument, "recess") == 0 || strcmp(argument, "reccess") == 0)
 {
 	if (show_system_messages) printf("\npoint ready_made_solution\n");
-	strcpy(SPT_word, "rëtsEs(s)");
+	strcpy(SPT_word, "r" SMALL_E_DIAERESIS "tsEs(s)");
 }
 else if (strcmp(argument, "bevid") == 0)
 {
 	if (show_system_messages) printf("\npoint ready_made_solution\n");
-	strcpy(SPT_word, "bëvId");
+	strcpy(SPT_word, "b" SMALL_E_DIAERESIS "vId");
 }
 else if (strcmp(argument, "perhvdi") == 0)
 {
@@ -46,15 +46,15 @@ else if (strcmp(argument, "perhvdi") == 0)
 else if (strcmp(argument, "perplex") == 0)
 {
 	if (show_system_messages) printf("\npoint ready_made_solution\n");
-	strcpy(SPT_word, "përplEks");
+	strcpy(SPT_word, "p" SMALL_E_DIAERESIS "rplEks");
 }
 else if (strcmp(argument, "parlament") == 0)
 {
 	if (show_system_messages) printf("\npoint ready_made_solution\n");
-	strcpy(SPT_word, "pArlamënt");
+	strcpy(SPT_word, "pArlam" SMALL_E_DIAERESIS "nt");
 }
 else if (strcmp(argument, "gouvernement") == 0)
 {
 	if (show_system_messages) printf("\npoint ready_made_solution\n");
-	strcpy(SPT_word, "gu:vErnëmënt");
+	strcpy(SPT_word, "gu:vErn" SMALL_E_DIAERESIS "m" SMALL_E_DIAERESIS "nt");
 }
