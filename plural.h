3ca60a77e716211f05454468ab0f0158f6187c5f
hmslima <hmslima@bol.com.br>
2020-03-29 14:45:24 -0300

    Delete plural.h

--- plural.h
+++
@@ -1,9 +0,0 @@
-#ifndef PLURAL_H
-#define PLURAL_H
-
-void if_it_is_a_thing (short int lang);
-void if_it_is_a_living_being (short int lang);
-void if_it_is_a_proper_name (short int lang);
-void plural ();
-
-#endif
